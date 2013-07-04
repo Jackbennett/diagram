8a9e7ea4c10d948ca5aa1a0a2600218605df21dc
Jack Bennett <me@jackben.net>
2013-07-04 21:07:35 +0100

    Charts work, bit laggy but what ya gonna do

--- js/diagram.js
+++ js/diagram.js
@@ -4,28 +4,20 @@
 	var diagram_div = div.find(".diagram");
 
 	// Setup the editor diagram
-	var editor = ace.edit(editor_div.get(0));
-	editor.setTheme("ace/theme/crimson_editor");
-	editor.getSession().setMode("ace/mode/asciidoc");
-	editor.getSession().on('change', _.debounce(on_change, 100));
+	// editor.getSession().setMode("ace/mode/asciidoc");
+	editor_div.on('keypress', _.debounce(on_change, 200));
 
 	on_change();
 
 	function on_change() {
 		try {
-			var diagram = Diagram.parse(editor.getValue());
-
-			editor.getSession().setAnnotations([]);
+			var diagram = Diagram.parse(editor_div.val());
 
 			// Clear out old diagram
 			diagram_div.html('');
 
-			var options = {
-				scale: 1
-		};
-
-		// Draw
-		diagram.drawSVG(diagram_div.get(0), options);
+			// Draw
+			diagram.drawSVG(diagram_div.get(0));
 
 		} catch(err) {
 			var annotation = {
@@ -38,19 +30,12 @@
 				annotation.row	= err.loc.first_line - 1;
 				annotation.column = err.loc.first_column;
 			}
-			editor.getSession().setAnnotations([annotation]);
+
 			throw err;
 		}
 	}
 }
 
-	$(document).ready(function() {
-		// Example diagrams
-		//$('.diagram').sequenceDiagram();
-
-		// Setup all the editors
-		setup_editor($('#demo'));
-		setup_editor($('#example1'));
-		setup_editor($('#example2'));
-		setup_editor($('#example3'));
-	});
+$(document).ready(function() {
+	setup_editor($('#pad'));
+});
