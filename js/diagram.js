daa3f289ec6249682b102830a1028ac92b99f198
Jack Bennett <me@jackben.net>
2013-07-04 20:30:52 +0100

    pulling out theme and download options, theme is default and don't need the download function

--- js/diagram.js
+++ js/diagram.js
@@ -2,8 +2,6 @@
 
 	var editor_div = div.find(".editor");
 	var diagram_div = div.find(".diagram");
-	var theme_div = div.find(".theme");
-	var download_link = div.find('.download');
 
 	// Setup the editor diagram
 	var editor = ace.edit(editor_div.get(0));
@@ -11,19 +9,6 @@
 	editor.getSession().setMode("ace/mode/asciidoc");
 	editor.getSession().on('change', _.debounce(on_change, 100));
 
-	download_link.click(function(ev) {
-	  var svg = diagram_div.find('svg')[0];
-	  var width = parseInt(svg.width.baseVal.value);
-	  var height = parseInt(svg.height.baseVal.value);
-	  var data = editor.getValue();
-	  var xml = '<?xml version="1.0" encoding="utf-8" standalone="no"?><!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 20010904//EN" "http://www.w3.org/TR/2001/REC-SVG-20010904/DTD/svg10.dtd"><svg xmlns="http://www.w3.org/2000/svg" width="' + width + '" height="' + height + '" xmlns:xlink="http://www.w3.org/1999/xlink"><source><![CDATA[' + data + ']]></source>' + svg.innerHTML + '</svg>';
-
-	  var a = $(this);
-	  a.attr("download", "diagram.svg"); // TODO I could put title here
-	  a.attr("href", "data:image/svg+xml," + encodeURIComponent(xml));
-	});
-
-	theme_div.change(on_change);
 	on_change();
 
 	function on_change() {
